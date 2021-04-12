1791456849a22deb82bf4e16464218c976e98b71
Christian A. Wolf <mail@cawolf.de>
2021-04-12 09:30:10 +0200

    fix: require composer.json from branch

--- Tests/Functional/Scripts/requireAdditionalVendors.sh
+++ Tests/Functional/Scripts/requireAdditionalVendors.sh
@@ -3,7 +3,12 @@ shopt -s extglob
 
 cd build/testproject/
 composer config extra.symfony.allow-contrib true
-composer require auxmoney/opentracing-bundle-amqplib-rabbitmq
+SUFFIX=""
+if [ -n "$GITHUB_REF" ];
+then
+    SUFFIX=:dev-${GITHUB_REF##*/}
+fi
+composer require auxmoney/opentracing-bundle-amqplib-rabbitmq${SUFFIX}
 rm -fr vendor/auxmoney/opentracing-bundle-amqplib-rabbitmq/*
 cp -r ../../!(build|vendor) vendor/auxmoney/opentracing-bundle-amqplib-rabbitmq
 composer dump-autoload
