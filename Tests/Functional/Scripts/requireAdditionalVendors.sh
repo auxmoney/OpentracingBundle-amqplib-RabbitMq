a0bedb9a548ec2adfc32873ce3c13417548041d2
Christian A. Wolf <mail@cawolf.de>
2021-04-12 09:54:29 +0200

    fix: revert to breaking functionals

--- Tests/Functional/Scripts/requireAdditionalVendors.sh
+++ Tests/Functional/Scripts/requireAdditionalVendors.sh
@@ -3,16 +3,7 @@ shopt -s extglob
 
 cd build/testproject/
 composer config extra.symfony.allow-contrib true
-SUFFIX=""
-if [ -n "$GITHUB_REF" ];
-then
-    SUFFIX=:dev-${GITHUB_REF##*/}
-fi
-if [ -n "$GITHUB_HEAD_REF" ];
-then
-    SUFFIX=:dev-${GITHUB_HEAD_REF##*/}
-fi
-composer require auxmoney/opentracing-bundle-amqplib-rabbitmq${SUFFIX}
+composer require auxmoney/opentracing-bundle-amqplib-rabbitmq
 rm -fr vendor/auxmoney/opentracing-bundle-amqplib-rabbitmq/*
 cp -r ../../!(build|vendor) vendor/auxmoney/opentracing-bundle-amqplib-rabbitmq
 composer dump-autoload
