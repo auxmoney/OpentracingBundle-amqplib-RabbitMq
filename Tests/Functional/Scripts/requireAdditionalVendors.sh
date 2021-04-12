2372e81080c123e131bdd3fff430942127d27c6e
Christian A. Wolf <mail@cawolf.de>
2021-04-12 09:44:35 +0200

    fix: require composer.json from branch for pull requests

--- Tests/Functional/Scripts/requireAdditionalVendors.sh
+++ Tests/Functional/Scripts/requireAdditionalVendors.sh
@@ -8,6 +8,10 @@ if [ -n "$GITHUB_REF" ];
 then
     SUFFIX=:dev-${GITHUB_REF##*/}
 fi
+if [ -n "$GITHUB_HEAD_REF" ];
+then
+    SUFFIX=:dev-${GITHUB_HEAD_REF##*/}
+fi
 composer require auxmoney/opentracing-bundle-amqplib-rabbitmq${SUFFIX}
 rm -fr vendor/auxmoney/opentracing-bundle-amqplib-rabbitmq/*
 cp -r ../../!(build|vendor) vendor/auxmoney/opentracing-bundle-amqplib-rabbitmq
